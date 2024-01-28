47f0dc675ec8bf02aababa912d9e523607ab83d7
Dhanush Pandian D <dhanushdpandian@gmail.com>
2024-01-28 08:31:56 +0530

    Update index.js

--- mct/backend/src/index.js
+++ mct/backend/src/index.js
@@ -7,23 +7,63 @@ const { User } = require("./models/user.model.js");
 app.use(cors());
 app.use(express.json());
 
-app.post("/create", async (req, res) => {
-  bank_id = req.body.bank_id
-  wallet_id = req.body.wallet_id
-  username = req.body.username
-  password = req.body.password
+// app.post("/create", async (req, res) => {
+//   bank_id = req.body.bank_id
+//   wallet_id = req.body.wallet_id
+//   username = req.body.username
+//   password = req.body.password
+//   const resp = await User.create({
+//     bank_id: bank_id,
+//     wallet_id: wallet_id,
+//     username: username,
+//     password: password,
+//   });
 
-  const resp = await User.create({
-    bank_id: bank_id,
-    wallet_id: wallet_id,
-    username: username,
-    password: password,
-  });
+//   return res.json({
+//     body: resp,
+//   });
+// });
+const checkUserExistence = async (req, res, next) => {
+  const { bank_id, wallet_id, username, password } = req.body;
 
-  return res.json({
-    body: resp,
-  });
+  if (!bank_id || !wallet_id || !username || !password) {
+    return res.status(400).json({ error: "Invalid parameters" });
+  }
+
+  try {
+    const userExists = await User.find({ bank_id, wallet_id });
+
+    if (userExists) {
+      return res.status(409).json({ error: "User already exists" });
+    }
+    next();
+  } catch (error) {
+    console.error("Error checking user existence:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+app.post("/create", checkUserExistence, async (req, res) => {
+  const { bank_id, wallet_id, username, password } = req.body;
+
+  try {
+    const resp = await User.create({
+      bank_id: bank_id,
+      wallet_id: wallet_id,
+      username: username,
+      password: password,
+    });
+
+    return res.json({
+      body: resp,
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
+
+
 
 app.post("/verify", async(req, res) => {
   pass = req.body.password
@@ -43,26 +83,46 @@ app.post("/verify", async(req, res) => {
 })
 
 app.post("/isuser", async (req, res) => {
-  console.log(req.body)
   const wallet_id =  req.body.wallet_id
-  const id = await User.exists({wallet_id: wallet_id})
-  if(id !== null ){
-    res.send(
-      {
-        flag: true
+  try{
+    const id = await User.exists({wallet_id: wallet_id})
+    if(id !== null ){
+      res.send(
+        {
+          flag: true
+        }
+        )
+      } 
+      else{
+        res.send(
+          {
+            flag: false
+          }
+          )
+        }
       }
-    )
-  } 
-  else{
-    res.send(
-      {
-        flag: false
+      catch (error) {
+        console.error("Error finding user:", error);
+        res.status(500).json({ error: "Internal Server Error" });
       }
-    )
-  }
-
-
 })
+
+app.post("/finduser", async (req, res) => {
+  const wallet_id = req.query.wallet_id; 
+  try {
+    const user = await User.findOne({ wallet_id }, "username");
+    if (user && user.username) {
+      res.json({ username: user.username });
+    } else {
+      res.json({ username: null }); 
+    }
+  } catch (error) {
+    console.error("Error finding user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
 module.exports = {
   app,
 };
